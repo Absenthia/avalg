da4796960e86f5e1187ebba4e5ce31f9b4289b14
Sebastian Grahn <segrahn@kth.se>
2014-11-16 15:42:24 +0100

    Added resultprinter to generator
    Generator can now print a result given a finished hashmap

--- src/Generator.java
+++ src/Generator.java
@@ -2,9 +2,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 
 public class Generator {
+	PrintWriter pnrwr, reswr;
 	BigInteger pnr;
 	int j;
 	
@@ -14,20 +17,36 @@ public class Generator {
 	    g.printerino();
 	}
 	
-	public Generator(BigInteger pnr, int j){
+	public Generator(BigInteger pnr, int j) throws IOException{
 		this.pnr = pnr;
 		this.j = j;
+		pnrwr = new PrintWriter(new FileWriter(pnr+".txt"));
+		reswr = new PrintWriter(new FileWriter(pnr+"_res.txt"));
+		reswr.println(pnr+" "+j);
+		reswr.flush();
 	}
 	
 	public void printerino() throws IOException{
-		PrintWriter pw = new PrintWriter(new FileWriter(pnr+".txt"));
 		BigInteger exponent = BigInteger.valueOf(10);
 		exponent = exponent.pow(60+j);
 		pnr = pnr.multiply(exponent);
 		for(int i=0; i<100; i++){
 			pnr = pnr.add(BigInteger.ONE);
-			pw.println(pnr.toString());
+			pnrwr.println(pnr.toString());
 		}
-		pw.flush();
+		pnrwr.flush();
+	}
+	
+	public void printResult(HashMap<String, String> res){
+		if(res == null){
+			reswr.println();
+		}else{
+			for(Entry<String, String> entry : res.entrySet()){
+				String key = entry.getKey();
+				String value = entry.getValue();
+				reswr.print(key+" "+value+" ");
+			}
+		}
+		reswr.flush();
 	}
 }
