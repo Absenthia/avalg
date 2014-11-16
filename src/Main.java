a5d32ea37d7c47328f61a313a3e4b7afb631036e
AntiChristoffer <christoffer.dahlgren@gmail.com>
2014-11-16 12:52:57 +0100

    Added big integer square root method

--- src/Main.java
+++ src/Main.java
@@ -270,5 +270,19 @@ public class Main {
     	result[1] = p.subtract(r);
     	return result;
     }
+    
+    public static BigInteger sqrt(BigInteger x) {
+        BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
+        BigInteger div2 = div;
+        // Loop until we hit the same value twice in a row, or wind
+        // up alternating.
+        for(;;) {
+            BigInteger y = div.add(x.divide(div)).shiftRight(1);
+            if (y.equals(div) || y.equals(div2))
+                return y;
+            div2 = div;
+            div = y;
+        }
+    }
 
 }
