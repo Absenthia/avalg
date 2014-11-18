9bbba193dd823d8f36ac4dad571ba8a8ef2f5880
Sebastian Grahn <segrahn@kth.se>
2014-11-18 19:52:38 +0100

    ubermain

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-	static final int NUM_THREADS = 6;
+	static final int NUM_THREADS = 2;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -24,8 +24,9 @@ public class Main {
 	    while(running){
 	    	System.out.println("Choose method:");
 	    	System.out.println("1: Run all");
-	    	System.out.println("2: Run specific interval");
-	    	System.out.println("3: Exit program");
+	    	System.out.println("2: Run all for specific pnr");
+	    	System.out.println("3: Run specific interval for specific pnr");
+	    	System.out.println("4: Exit program");
 	    	
 	    	int choice = Integer.parseInt(b.readLine());
 	    	if(choice == 1){
@@ -38,6 +39,15 @@ public class Main {
 	    	}else if(choice == 2){
 	    		System.out.println("Enter pnr");
 	    		String pNum = b.readLine();
+	    		System.out.println("Enter J value");
+	    		int J = Integer.parseInt(b.readLine());
+	    		System.out.println("Enter runtime in minutes");
+	    		int runTime = Integer.parseInt(b.readLine());
+	    		
+	    		m.runAllOne(pNum, J, runTime);
+	    	}else if(choice == 3){
+	    		System.out.println("Enter pnr");
+	    		String pNum = b.readLine();
 	    		System.out.println("Enter start point");
 	    		int startval = Integer.parseInt(b.readLine());
 	    		System.out.println("Enter end point");
@@ -48,7 +58,7 @@ public class Main {
 	    		int runTime = Integer.parseInt(b.readLine());
 	    		
 	    		m.runChosen(pNum, startval, stopval, j, runTime);
-	    	}else if(choice == 3){
+	    	}else if(choice == 4){
 	    		running = false;
 	    	}
 	    }
@@ -83,49 +93,21 @@ public class Main {
     	rebuildFile(pNum,startval,stopval, J);
     }
     
+    public void runAllOne(String pNum, int J, int runTime) throws InterruptedException, IOException{
+    	int startval = 1;
+    	int stopval = 100;
+    	
+    	runChosen(pNum, startval, stopval, J, runTime);
+    }
+    
     public void runAll(int J, int runTime) throws IOException, InterruptedException{
     	//RUN CHD PNR
     	String pNum = "7910200059";
-    	
-    	int startval = 1;
-    	int stopval = 5;
-    	int length = 5;
-    	int intervals = 100/length;
-    	int runMin = runTime;
-    	int runSec = runMin*60;
-    	
-    	ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
-    	int start = startval;
-    	int stop = stopval;
-    	for(int i=0; i<intervals; i++){
-    		Brent worker = new Brent(pNum, start, stop, J, runSec);
-    		executor.execute(worker);
-    		start += length;
-    		stop += length;
-    	}
-    	//Finish all threads in queue
-    	executor.shutdown();
-    	executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
-    	
-    	rebuildFile(pNum,startval,stopval, J);
+    	runAllOne(pNum, J, runTime);    	
     	
     	//RUN SEB PNR
     	pNum = "9106175632";
-    	
-    	ExecutorService executor2 = Executors.newFixedThreadPool(NUM_THREADS);
-    	start = startval;
-    	stop = stopval;
-    	for(int i=0; i<intervals; i++){
-    		Brent worker = new Brent(pNum, start, stop, J, runSec);
-    		executor2.execute(worker);
-    		start += length;
-    		stop += length;
-    	}
-    	//Finish all threads in queue
-    	executor2.shutdown();
-    	executor2.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
-    	
-    	rebuildFile(pNum,startval,stopval, J);
+    	runAllOne(pNum, J, runTime);    
     }
     
     public void rebuildFile(String pNum, int x, int y, int J) throws IOException{
